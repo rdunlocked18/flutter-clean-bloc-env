8ccf79120f74effe848db01b7ed904373d6a2e0d
Rohit Daftari <rohitdaftari3@gmail.com>
2023-06-16 19:05:46 +0530

    Add DI Configs to Other Envs

--- lib/main/main_dev.dart
+++ lib/main/main_dev.dart
@@ -1,4 +1,12 @@
+import 'package:flutter/material.dart';
+
 import '../main.dart';
 import 'app_env.dart';
+import '../injection_container.dart' as di;
 
-Future<void> main() async => mainCommon(AppEnvironment.DEV);
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+
+  mainCommon(AppEnvironment.DEV);
+}
--- lib/main/main_staging.dart
+++ lib/main/main_staging.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/material.dart';
+
 import '../main.dart';
 import 'app_env.dart';
+import '../injection_container.dart' as di;
 
-Future<void> main() async => mainCommon(AppEnvironment.STAGING);
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  mainCommon(AppEnvironment.STAGING);
+}
